517460ac4875a2a56dbe317ae3e41626e37b47df
CesarOli <cesaraleoliveira@gmail.com>
2023-09-01 18:32:11 -0300

    Estrutura básica da tela 'cadastro'.

--- cadastro.py
+++ cadastro.py
@@ -1,4 +1,11 @@
-from PyQt5 import ui,QtWidgets
+from PyQt5 import uic, QtWidgets
 
 def cadastro_de_jogos():
     print('Funcionou!!')
+
+app = QtWidgets.QApplication([])
+cadastro = uic.loadUi('cadastro.ui')
+cadastro.pushButton.clicked.connect(cadastro_de_jogos)  # Correção aqui
+
+cadastro.show()
+app.exec()
