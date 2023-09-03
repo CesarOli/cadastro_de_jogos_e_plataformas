fe81844ffae96d3f8ca241176a0bba12b82a233d
CesarOli <cesaraleoliveira@gmail.com>
2023-09-02 23:55:23 -0300

    Criação estrura condicionais

--- cadastro.py
+++ cadastro.py
@@ -1,11 +1,22 @@
 from PyQt5 import uic, QtWidgets
+from time import sleep
 
 def cadastro_de_jogos():
-    print('Funcionou!!')
+    nome_do_jogo = cadastro.lineEdit.text()
+    ano_lancamento = cadastro.lineEdit_2.text()
+
+    if cadastro.radioButton.isChecked():
+        
+    elif cadastro.radioButton_2.isChecked():
+        
+    elif cadastro.radioButton_3.isChecked():
+        
+    else:
+        
 
 app = QtWidgets.QApplication([])
 cadastro = uic.loadUi('cadastro.ui')
-cadastro.pushButton.clicked.connect(cadastro_de_jogos)  # Correção aqui
-
+cadastro.pushButton.clicked.connect(cadastro_de_jogos)
+sleep(1.5)
 cadastro.show()
 app.exec()
