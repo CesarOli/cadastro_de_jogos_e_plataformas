c00b44d2162259ff3129f6f44c0ce9024ca41d84
CesarOli <cesaraleoliveira@gmail.com>
2023-09-09 22:18:13 -0300

    Retirada do Cabeçalho com os campos da tabela.

--- cadastro.py
+++ cadastro.py
@@ -14,7 +14,6 @@ banco_de_dados = mysql.connector.connect(
 tela_lista_de_jogos = uic.loadUi('lista_de_jogos.ui')
 tabela = tela_lista_de_jogos.tableWidget
 
-
 def cadastro_de_jogos():
     nome_do_jogo = cadastro.lineEdit.text()
     ano_lancamento = cadastro.lineEdit_2.text()
@@ -66,19 +65,6 @@ def chama_tela_lista_de_jogos():
     seleciona_a_tabela = "SELECT * FROM  Games"
     cursor.execute(seleciona_a_tabela)
     dados_recebidos = cursor.fetchall()
-
-    colunas = ['ID', 'Nome_do_Jogo', 'Ano_Lancamento', 'Plataforma']
-
-    tela_lista_de_jogos.tableWidget.setRowCount(len(dados_recebidos) + 1)
-    tela_lista_de_jogos.tableWidget.setColumnCount(len(colunas))
-
-    for j in range(len(colunas)):
-        tela_lista_de_jogos.tableWidget.setItem(0, j, QtWidgets.QTableWidgetItem(colunas[j]))
-
-    for i in range(len(dados_recebidos)):
-        for j in range(len(dados_recebidos[i])):
-            item = dados_recebidos[i][j] if j < len(dados_recebidos[i]) else ""
-            tela_lista_de_jogos.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(item)))
 
     
 cadastro = uic.loadUi('cadastro.ui')
